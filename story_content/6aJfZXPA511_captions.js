752d18e35a0061e05a5fd69d2498021bf198ca7e
nana0401 <nurnazhifah04@gmail.com>
2025-01-20 23:17:33 +0700

    Sistem Peredaran Darah Manusia

---
+++ story_content/6aJfZXPA511_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.17.27621.0%0D%0A%0D%0A00:00:07.768%20--%3E%2000:00:13.088%0D%0A%0D%0A%0D%0A00:00:13.120%20--%3E%2000:00:19.136%0D%0A%0D%0A%0D%0A00:00:19.168%20--%3E%2000:00:20.704%0D%0A%0D%0A%0D%0A"}')
